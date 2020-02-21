be3b2e85651a2f8514d488402d88721d9ad17b5b
thedrowsywinger <abdullah.md.sarwar@gmail.com>
2020-02-21 07:40:26 +0600

    added necessary files

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# word_frequency_checker
+# Word Frequency Checker
+
+This project was created for an assignment required by the course CSE 225 ("Data Structure and Algorithms") at North South University. 
+
+# Features!
+Given a text file as input:
+  - Shows the frequency of each word
+  - Shows the position of the words
+  - Information for each unique word is only shown once
+
+### Task
+
+This assignment required us to write a program which accepts the name of a text file from the command line. It can be assumed that the text file contains English text. The program should then output the frequency of each word in the file, along with a list of position indexes. The information for each unique word should be shown only once.
+
+If for example the input text file contains the following content:
+
+> The quick brown fox jumps over the lazy dog. However, the dog really did not care.
+
+The output should be the following:
+
+>Total number of words: 16
+
+>Word Frequencies and Positions:
+
+> the - 0.1875: 1,7,11
+> quick - 0.0625: 2
+> brown - 0.0625: 3
+> fox - 0.0625: 4
+> jumps - 0.0625: 5
+> over - 0.0625: 6 
+> lazy - 0.0625: 8
+> dog - 0.125: 9, 12
+> however - 0.0625: 10
+> really - 0.0625: 13 
+> did - 0.0625: 14 
+> not - 0.0625: 15 
+> care - 0.0625: 16 
+
+
+It must be noted this example is extremely small. The program should be written so that it can handle an arbitrary number of words, appearing in an arbitrary number of positions. The program must also reliably work with files larger than 1 MB. 
+
+### How to run this code
+
+```sh
+$ g++ frequency_checker.cpp -o run
+$ ./run
+$ Type the name of your file: input
+```
+
---
+++ frequency_checker.cpp
@@ -0,0 +1,169 @@
+#include<iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct pos {
+    int index;
+    pos* next;
+};
+
+struct node {
+    string data;
+    int counter;
+    node *next;
+    pos *position;
+};
+
+int MainIndex = 0;
+int totalWord=0;
+node* head =  NULL;
+//pos *h2 =  NULL;
+//int counter = 0;
+
+
+node* CreateNode(string data){
+    node *temp = new node;
+    temp->data = data;
+    temp->counter = 0;
+    temp->next = NULL;
+    temp->position = NULL;
+    return temp;
+}
+
+pos* insertPosition(pos* positionRoot, int indx){
+
+    pos* Ptemp = new pos();
+    Ptemp->index = indx;
+    Ptemp->next = NULL;
+    if(positionRoot == NULL){
+        positionRoot=Ptemp;
+        return positionRoot;
+    }
+    else{
+        pos *Current  = positionRoot;
+        while (Current->next != NULL){
+            Current =Current->next;
+
+        }
+        Current ->next =Ptemp;
+        return positionRoot;
+
+    }
+
+
+}
+
+void addNodeToTheList(string data){
+    node *temp = new node();
+    temp->data = data;
+    temp->counter = 0;
+    temp->next = NULL;
+    temp->position = NULL;
+
+
+    if(head == NULL){
+        temp->counter++;
+        temp->position = insertPosition(temp->position,++MainIndex);
+
+        head=temp;
+        //return head;
+    }
+    else{
+
+            node* tempHead=head;
+
+        while(tempHead->next!=NULL )
+        {
+
+
+            if((tempHead->data.compare(data) ==0)){
+                break;
+            }
+            tempHead=tempHead->next;
+        }
+
+        if((tempHead->data.compare(data) ==0))
+        {
+            tempHead->counter++;
+            tempHead->position= insertPosition(tempHead->position, ++MainIndex);
+            return;
+        }
+        else{
+
+            temp->counter++;
+            temp->position= insertPosition(temp->position, ++MainIndex);
+            tempHead->next=temp;
+        }
+    }
+
+}
+
+void printTHE(){
+    double freq=0;
+    node *something = head;
+    while(something != NULL){
+
+        freq = (double) something->counter/totalWord;
+
+        cout<<"Word: "<<something->data <<" - Frequency: "<<freq <<" - Position: ";
+        pos* mPos = something->position;
+
+        while(mPos!=NULL)
+        {
+            cout << mPos->index << " " ;
+            mPos=mPos->next;
+        }
+        cout <<endl;
+
+        something=something->next;
+
+    }
+}
+
+int main(void) {
+    ifstream file;
+
+
+    // file.open("input.txt", std::fstream::out);
+
+    string x, y ; 
+    cout << "Type the name of your file: "; 
+    cin >> x; 
+    
+
+    y = x.append(".txt");
+    cout << "Opening " << y; 
+
+    file.open(y, std::fstream::out);
+
+    string s;
+    if (file.is_open()) {
+
+        while (file >> s) {
+
+            transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+            int sizes=s.size();
+            if(s[sizes-1] == ',' || s[sizes-1] == '.')
+            {
+                string newWord(&s[0], &s[sizes-1]);
+                s=newWord;
+            }
+
+
+
+            addNodeToTheList(s);
+
+
+            totalWord++;
+
+
+        }
+        printTHE();
+
+
+    }
+}
